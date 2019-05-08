cc64b1819f2049eee41ca9d60e63ac9fa4bff85c
Amit <amitkamat94@gmail.com>
2019-05-07 23:42:02 -0400

    Update sets-symmetric_difference.py

--- sets-symmetric_difference.py
+++ sets-symmetric_difference.py
@@ -5,7 +5,7 @@ url = https://www.hackerrank.com/challenges/py-set-symmetric-difference-operatio
 a = input()
 list_eng = set(map(int, input().split()))
 
-b = input
+b = input()
 list_fre = set(map(int, input().split()))
 
 print(len(list_eng ^ list_fre))
