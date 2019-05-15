53b917d9a12b0621e0e21264f19cc935cc429985
Amit <amitkamat94@gmail.com>
2019-05-15 03:04:37 -0400

    Update and rename basic-data-types-list-comprehensions to basic-data-types-list-comprehensions.py

--- basic-data-types-list-comprehensions.py
+++ basic-data-types-list-comprehensions.py
@@ -2,9 +2,8 @@
 url = https://www.hackerrank.com/challenges/list-comprehensions/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 '''
 
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
 print([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a+b+c != n])
